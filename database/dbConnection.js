d367a9d123ed073a1f62dc27352719d3333cfff9
annu-adi <rashmi.maurya10@gmail.com>
2026-07-13 19:09:53 +0530

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
-export const dbConnection = async()=>{
-   await mongoose.connect(process.env.MONGO_URI,{
-        dbName:"MERN_STCK_HOSPITAL_MANAGEMENT_SYSTEM"
-    }).then(()=>{
-        console.log("Connection to database!")
-    }).catch((err) => {
-        console.log(`Some error occured while connecting to database:${err}`);
-    });
+
+
+   export const dbConnection = async () =>{
+    try{
+     await mongoose.connect(process.env.MONGO_URI)
+     console.log("DB connected")   
+    }catch(error){
+        console.log("db error")
+    }
 }
