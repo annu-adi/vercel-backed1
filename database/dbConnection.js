0b122965a8d5bae12f8cd3e26ce9e5425f0233e8
annu-adi <rashmi.maurya10@gmail.com>
2026-07-13 19:29:02 +0530

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 
 
 
-   export const dbConnection = async () =>{
-    try{
-     await mongoose.connect("mongodb://rashmi:rashmi123@cluster0-shard-00-00.pszpa.mongodb.net:27017,cluster0-shard-00-01.pszpa.mongodb.net:27017,cluster0-shard-00-02.pszpa.mongodb.net:27017/portal?ssl=true&replicaSet=atlas-cfodzj-shard-0&authSource=admin&appName=Cluster0")
-     console.log("DB connected")   
-    }catch(error){
-        console.log("db error")
-    }
+   export const dbConnection=()=>{
+    mongoose.connect('mongodb://rashmi:rashmi123@cluster0-shard-00-00.pszpa.mongodb.net:27017,cluster0-shard-00-01.pszpa.mongodb.net:27017,cluster0-shard-00-02.pszpa.mongodb.net:27017/portal?ssl=true&replicaSet=atlas-cfodzj-shard-0&authSource=admin&appName=Cluster0').then(()=>{
+        console.log("Db Connected");
+
+    }).catch((error)=>{
+       console.log(error);
+   })
+    
 }
