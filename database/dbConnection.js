b77aaafb2064e8ff0ab43e4e46e2be2c0a33753d
annu-adi <rashmi.maurya10@gmail.com>
2026-07-13 19:36:16 +0530

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 
    export const dbConnection=()=>{
-    mongoose.connect('mongodb://rashmi:rashmi123@cluster0-shard-00-00.pszpa.mongodb.net:27017,cluster0-shard-00-01.pszpa.mongodb.net:27017,cluster0-shard-00-02.pszpa.mongodb.net:27017/portal?ssl=true&replicaSet=atlas-cfodzj-shard-0&authSource=admin&appName=Cluster0').then(()=>{
+    mongoose.connect('mongodb+srv://rashmi:<db_password>@cluster0.pszpa.mongodb.net/?appName=Cluster0').then(()=>{
         console.log("Db Connected");
 
     }).catch((error)=>{
