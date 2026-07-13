3c70285b7c2b721ef3d981d819d6b4f456fb02e2
annu-adi <rashmi.maurya10@gmail.com>
2026-07-13 19:45:22 +0530

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 
    export const dbConnection=()=>{
-    mongoose.connect('mongodb+srv://rashmi:<rashmi123>@cluster0.pszpa.mongodb.net/portal?appName=Cluster0').then(()=>{
+    mongoose.connect('mongodb+srv://rashmi:rashmi123@cluster0.pszpa.mongodb.net/portal?appName=Cluster0').then(()=>{
         console.log("Db Connected");
 
     }).catch((error)=>{
