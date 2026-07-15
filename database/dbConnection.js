989d31952b334a8d4d6be5ec493c3006cc191a3f
annu-adi <rashmi.maurya10@gmail.com>
2026-07-15 19:29:34 +0530

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 
 
    export const dbConnection=()=>{
-    mongoose.connect('mongodb+srv://rashmi:rashmi123@cluster0.pszpa.mongodb.net/portal?appName=Cluster0').then(()=>{
+    mongoose.connect('mongodb+srv://rashmi:rashmi123@cluster0.pszpa.mongodb.net/portal?appName=Cluster0',
+                    serverSelectionTimeoutMS:5000,
+                    socketTimeoutMS:45000,
+       ).then(()=>{
         console.log("Db Connected");
 
     }).catch((error)=>{
